09e472ae914fa94cc0a0da805913065da59d70c0
Vivek Patil <patilv@gmail.com>
2020-04-13 06:56:02 -0700

    updated apps

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -7,23 +7,22 @@ shinyServer(function(input, output) {
                             iris$Sepal.Width>=input$Sepal.Width[1] & iris$Sepal.Width<=input$Sepal.Width[2]&
                                iris$Petal.Length>=input$Petal.Length[1] & iris$Petal.Length<=input$Petal.Length[2]&
                                   iris$Petal.Width>=input$Petal.Width[1] & iris$Petal.Width<=input$Petal.Width[2]),])
-                            
-  
-    output$subsetdata <- renderDataTable(
+
+
+    output$subsetdata <- DT::renderDataTable(
     dataset(),options=list(pageLength=10))
-  
-  
+
+
   output$summary <- renderPrint(
     summary(dataset())
   )
-  
+
   output$scatterplot=renderPlot(ggplot(dataset(),aes_string(x=input$xvar, y=input$yvar,color="Species"))+ geom_point()+
     ggtitle("Scatter Plot")
     )
-  
+
   output$boxplot=renderPlot(ggplot(dataset(),aes_string(x="Species",y=input$dvar))+ geom_boxplot()+
-      ggtitle("Box Plot")
+                              ggtitle("Box Plot")
   )
-  
+
 })
-  
